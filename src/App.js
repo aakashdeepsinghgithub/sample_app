7ac766ded7401a3705a5a74203b3e749f3d09537
aakashdeepsinghgithub <deepaakash0281@gmail.com>
2024-12-05 00:49:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentName from './sample'
+function App() {
+  return (
+    <>
+    <ComponentName/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentName = () => {
+  return (
+    <div>
+      <h1>Hello, this i an example to deploy react app </h1>
+    </div>
+  );
+};
+
+export default ComponentName;
